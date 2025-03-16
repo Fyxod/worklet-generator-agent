7e2b1dbf3f85436c23045c1f048cf8aa17b47baa
Parth Katiyar <parthdev0101@gmail.com>
2025-03-16 13:31:39 +0530

    initial commit

---
+++ api/main.py
@@ -0,0 +1,49 @@
+from fastapi import FastAPI, UploadFile, File
+from typing import Annotated
+import os
+import aiofiles
+from datetime import datetime
+from pydantic import BaseModel
+
+class Query(BaseModel):
+    query: str
+
+UPLOAD_DIR="../worklets"
+os.makedirs(UPLOAD_DIR, exist_ok=True)
+
+app = FastAPI()
+
+@app.get('/')
+def read_root():
+    print("here")
+    return {
+        2:"yo"
+    }
+    
+@app.post('/upload')
+async def upload_multiple(files: Annotated[list[UploadFile], File()]):
+    print("printong",files)
+    
+    saved_files = []
+
+    for file in files:
+        timestamp = int(datetime.now().timestamp() * 1000)  
+
+        filename, ext = os.path.splitext(file.filename)
+
+        new_filename = f"{filename}-{timestamp}{ext}"  
+        file_path = os.path.join(UPLOAD_DIR, new_filename)
+
+        async with aiofiles.open(file_path, "wb") as buffer:
+            content = await file.read() 
+            await buffer.write(content)
+
+        saved_files.append(file_path)
+
+    return {"saved_files": saved_files}
+
+@app.post('/query')
+async def create_query(query:Query):
+    print(query.query)
+    
+    return query.query
