a19d2718492d85d58eff3de4ad28dd938923aa74
Parth Katiyar <parthdev0101@gmail.com>
2025-04-11 10:25:57 +0530

    Refactor invoke_llm function to support Ollama model invocation and enhance response handling

--- app/llm.py
+++ app/llm.py
@@ -1,6 +1,9 @@
 from dotenv import load_dotenv
 from langchain_google_genai import ChatGoogleGenerativeAI
+import requests
 import os
+import json
+import re
 load_dotenv()
 
 llm = ChatGoogleGenerativeAI(
@@ -69,18 +72,40 @@ llm2 = ChatGoogleGenerativeAI(
 # # response = llm.invoke([HumanMessage(content="What is the capital of France?")])
 # # print(response.content)
 
+ollama_models = [
+    "gemma3:27b",
+    "gemma3:latest",
+    "deepseek-r1:70b",
+    "llama3.3:latest"
+]
+
+def invoke_llm(prompt, model):
+
+    raw_text = ""
+    if model in ollama_models:
+        print("Using Ollama")
+        payload = {"prompt": prompt}
+        url = f"{os.getenv('LLM_URL')}/query?model={model}"
+        response = requests.post(url, json=payload)
+
+        # Parse response JSON
+        data = json.loads(response.content)
+        raw_text = data.get("content", "")
 
 
-# def invoke_llm(prompt, model):
+        return cleaned
 
-#     if llm_switch == "ollama":
-#         payload = {"prompt": prompt}
-#         url = f"{os.getenv('LLM_URL')}/query?model={model}"
-#         response = requests.post(url, json=payload) 
-#         return{"content":response.json()['content'].strip()}
+    else:
+        print("Using Google Gemini")
+        response = llm.invoke(prompt)
+        raw_text = response.content
+    # Clean: remove markdown symbols and normalize whitespace
+    cleaned = re.sub(r"<think>.*?</think>", "", raw_text, flags=re.DOTALL | re.IGNORECASE)
+    cleaned = re.sub(r"\*+", "", cleaned)  # remove markdown asterisks
+    cleaned = re.sub(r"#", "", cleaned)  # Remove hash symbols
+    cleaned = re.sub(r"\s+", " ", cleaned) # normalize whitespace  # Normalize all whitespace
+    cleaned = cleaned.strip()
 
-#     elif llm_switch == "gemini":
-#         response = llm.invoke(prompt)
-#         return response
+    return cleaned
 
-# # print(invoke_llm("What is the capital of France?"))
+print(invoke_llm("what is the capital of france", "llama3.3:latest"))
