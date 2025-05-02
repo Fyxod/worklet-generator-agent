5b4cf1d514b5c46906de43c379b75fbbe353e31c
Parth Katiyar <parthdev0101@gmail.com>
2025-05-02 11:37:09 +0530

    Refactor llm.py and socket.py by removing unused models and print statements; delete links_eg.json for cleanup.

--- app/main.py
+++ app/main.py
@@ -1,10 +1,7 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from app.routers import root
-import subprocess
 from fastapi.middleware.cors import CORSMiddleware
-import sys
-import os
 import socketio
 from app.socket import sio
 
@@ -12,18 +9,17 @@ from app.socket import sio
 fastapi_app = FastAPI()
 
 # Add CORS middleware
-fastapi_app.add_middleware(
+fastapi_app.add_middleware( # allowed all origins for now
     CORSMiddleware,
-    allow_origins=["*"],  # Allow all origins
+    allow_origins=["*"],
     allow_credentials=True,
-    allow_methods=["*"],  # Allow all methods (GET, POST, etc.)
-    allow_headers=["*"],  # Allow all headers
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 # Mount static files (serve 'app/public' at '/static')
 fastapi_app.mount("/static", StaticFiles(directory="app/public"), name="static")
 
-# Include your router
 fastapi_app.include_router(root.router)
 
 # Wrap the FastAPI app with the Socket.IO server
