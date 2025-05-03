fe519fe46ddfd2dde068bf3c39099ea5749719c5
Parth Katiyar <parthdev0101@gmail.com>
2025-05-04 01:00:26 +0530

    Refactor API key and search engine ID order in google_search.py; update placeholder logic in index.html for better user experience

--- app/utils/search_functions/google_search.py
+++ app/utils/search_functions/google_search.py
@@ -4,13 +4,17 @@ import json
 from dotenv import load_dotenv
 load_dotenv()
 api_keys = [
-    os.getenv("GOOGLE_API_KEY2"),
+    os.getenv("GOOGLE_API_KEY3"),
+    os.getenv("GOOGLE_API_KEY4"),
     os.getenv("GOOGLE_API_KEY1"),
+    os.getenv("GOOGLE_API_KEY2"),
 ]
 
 search_engine_ids = [
-    os.getenv("SEARCH_ENGINE_ID2"),
+    os.getenv("SEARCH_ENGINE_ID3"),
+    os.getenv("SEARCH_ENGINE_ID4"),
     os.getenv("SEARCH_ENGINE_ID1"),
+    os.getenv("SEARCH_ENGINE_ID2"),
 ]
 
 api_key_ref = os.getenv("GOOGLE_API_KEY_ref")
